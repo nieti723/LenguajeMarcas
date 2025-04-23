0e16ea44eee4dd8c431f53f8df3f93182de946c5
nieti723 <juan.nieto-acosta@iesruizgijon.com>
2025-04-23 08:55:51 +0200

    Terminado validación de formularios

--- javascript/ej2/script.js
+++ javascript/ej2/script.js
@@ -1,32 +1,41 @@
 function validarFormulario() {
     let legend = document.getElementById('legend');
-    let alert;
+    let alert = "";
     let cont = 0;
 
 
     if(!error('name')){
         cont++;
+        alert += "nombre ";
     }
     
     if (!error('lastName')) {
         cont++;
+        alert += "apellidos ";
     }
 
     if (!error('address')) {
         cont++;
+        alert += "dirección ";
     }
 
     if (!error('phoneNumber')) {
         cont++;
+        alert += "teléfono ";
     }
 
     if (!error('zipCode')) {
         cont++;
+        alert += "cp";
+    }
+
+    if (alert != null) {
+        alert = alert.replaceAll(" ", ", ");
     }
 
     if (cont>0) {
         legend.classList.add("error")
-        legend.innerHTML = "Debe rellenas todos los campos";
+        legend.innerHTML = "Debe rellenar los siguientes campos: " + alert;
         return false;
     } else{
         return true;
